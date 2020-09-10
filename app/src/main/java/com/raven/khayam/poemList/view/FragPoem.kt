fcd9c39b52a3a5a7bc4d738d475bd104ad65c192
smart8bits <smart8bits@gmail.com>
2020-09-10 12:17:45 +0430

    turn recycler view to view pager.

---
+++ app/src/main/java/com/raven/khayam/poemList/view/FragPoem.kt
@@ -0,0 +1,29 @@
+package com.raven.khayam.poemList.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.raven.khayam.R
+import com.raven.khayam.model.PoemItem
+
+class FragPoem private constructor(private val poemItem: PoemItem): Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragment = inflater.inflate(R.layout.poem_holder, container, false)
+        fragment.findViewById<TextView>(R.id.txtViwPoem).text = poemItem.text
+        return fragment
+    }
+
+    companion object{
+        fun create(poemItem: PoemItem): FragPoem {
+            return FragPoem(poemItem)
+        }
+    }
+}
--- app/src/main/java/com/raven/khayam/poemList/view/MainActivity.kt
+++ app/src/main/java/com/raven/khayam/poemList/view/MainActivity.kt
@@ -1,25 +1,27 @@
 package com.raven.khayam.poemList.view
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.raven.khayam.R
 import com.raven.khayam.di.DaggerViewModelComponent
 import com.raven.khayam.di.ViewModelFactory
 import com.raven.khayam.poemList.ViewModelPoemList
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
     private lateinit var viewModel: ViewModelPoemList
 
-    private lateinit var recViwPoemList: RecyclerView
-    private lateinit var poemAdapter: PoemAdapter
+    private lateinit var viewPager: ViewPager2
+    private lateinit var poemPagerAdapter: PoemFragStateAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,20 +41,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initiate() {
-        recViwPoemList = findViewById(R.id.RecViwPoemList)
-        poemAdapter = PoemAdapter(viewModel.poemList)
-        initiateUsersRecyclerView()
-    }
-
-    private fun initiateUsersRecyclerView() {
-        val linearLayoutManager = LinearLayoutManager(this)
-        recViwPoemList.apply {
-            layoutManager = linearLayoutManager
-            adapter = poemAdapter
-        }
+        viewPager = findViewById(R.id.pagerPoem)
+        poemPagerAdapter = PoemFragStateAdapter(this, viewModel.poemList)
+        viewPager.adapter = poemPagerAdapter
     }
 
     private fun setObservers() {
-        viewModel.showPoems.observe(this, Observer { poemAdapter.notifyDataSetChanged() })
+        viewModel.showPoems.observe(this, Observer { poemPagerAdapter.notifyDataSetChanged() })
     }
 }

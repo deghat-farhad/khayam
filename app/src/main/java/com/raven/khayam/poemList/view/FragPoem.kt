92404d1e4c2ce401a7cb5f7e3281d5d5a477be8e
smart8bits <smart8bits@gmail.com>
2020-09-12 11:46:04 +0430

    full screen.

--- app/src/main/java/com/raven/khayam/poemList/view/FragPoem.kt
+++ app/src/main/java/com/raven/khayam/poemList/view/FragPoem.kt
@@ -5,11 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.raven.khayam.R
 import com.raven.khayam.model.PoemItem
 
-class FragPoem private constructor(private val poemItem: PoemItem): Fragment() {
+class FragPoem private constructor(private val poemItem: PoemItem) : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,11 +18,29 @@ class FragPoem private constructor(private val poemItem: PoemItem): Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val fragment = inflater.inflate(R.layout.poem_holder, container, false)
-        fragment.findViewById<TextView>(R.id.txtViwPoem).text = poemItem.text
+        initiate(fragment)
         return fragment
     }
 
-    companion object{
+    private fun initiate(fragment: View){
+        fragment.findViewById<TextView>(R.id.txtViwPoem).text = poemItem.text
+        fragment.findViewById<CardView>(R.id.cardViwPoem).setOnClickListener { fullScreen() }
+    }
+
+    private fun fullScreen() {
+        activity?.let {
+            it.window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    )
+        }
+    }
+
+    companion object {
         fun create(poemItem: PoemItem): FragPoem {
             return FragPoem(poemItem)
         }
--- app/src/main/java/com/raven/khayam/poemList/view/MainActivity.kt
+++ app/src/main/java/com/raven/khayam/poemList/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.raven.khayam.poemList.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -42,6 +43,12 @@ class MainActivity : FragmentActivity() {
     }
 
     private fun initiate() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
         viewPager = findViewById(R.id.pagerPoem)
         //val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
         poemPagerAdapter = PoemFragStateAdapter(this, viewModel.poemList)

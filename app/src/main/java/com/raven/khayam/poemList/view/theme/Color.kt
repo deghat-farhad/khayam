131a986076f42155e82b6a1a2cc8125299470492
deghat-farhad <53361451+deghat-farhad@users.noreply.github.com>
2024-03-29 11:56:12 +0330

    icon changed. (#41)

--- app/src/main/java/com/raven/khayam/poemList/view/theme/Color.kt
+++ app/src/main/java/com/raven/khayam/poemList/view/theme/Color.kt
@@ -2,10 +2,14 @@ package com.raven.khayam.poemList.view.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val mutedReddishBrown = Color(0xFF734854)
+val creamyBeige = Color(0xFFF2F2E9)
+val earthyBrown = Color(0xFF736766)
+val mutedGrayishBrown = Color(0xFFD9D7C5)
+val mutedMauve = Color(0xFFA69580)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val mutedMidnightBlue = Color(0xFF0E1825)
+val deepIndigo = Color(0xFF1A2C40)
+val lightLavender = Color(0xFFEDE8FF)
+val mutedRose = Color(0xFFBF7885)
+val dustyRose = Color(0xFF8C4F65)

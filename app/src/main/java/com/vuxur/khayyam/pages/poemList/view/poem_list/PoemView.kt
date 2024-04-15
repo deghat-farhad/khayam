62b035a6323ca93de4351357a55d2c6bcd644d95
deghat-farhad <53361451+deghat-farhad@users.noreply.github.com>
2024-04-15 16:02:09 +0330

    no calculation for "PoemView" header Divider (#75)

--- app/src/main/java/com/vuxur/khayyam/pages/poemList/view/poem_list/PoemView.kt
+++ app/src/main/java/com/vuxur/khayyam/pages/poemList/view/poem_list/PoemView.kt
@@ -8,14 +8,10 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.NonRestartableComposable
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
@@ -25,33 +21,26 @@ import com.vuxur.khayyam.R
 import com.vuxur.khayyam.model.LocaleItem
 import com.vuxur.khayyam.model.PoemItem
 
+@NonRestartableComposable
 @Composable
 fun PoemView(
     modifier: Modifier = Modifier,
     poemItem: PoemItem,
     localeItem: LocaleItem.CustomLocale,
 ) {
-    val density = LocalDensity.current
-    var maxWidth by remember { mutableStateOf(0.dp) }
     val isFarsi = localeItem.locale.language == "fa"
 
-    val fontFamily = if (isFarsi)
+    val fontFamily = remember(isFarsi) {
         FontFamily(
             Font(
-                R.font.iran_sans_x_regular
+                if (isFarsi) R.font.iran_sans_x_regular else R.font.e_b_garamond_regular
             )
         )
-    else
-        FontFamily(
-            Font(
-                R.font.e_b_garamond_regular
-            )
-        )
+    }
 
-    val fontSize = if (isFarsi)
-        19.sp
-    else
-        18.sp
+    val fontSize = remember(isFarsi) {
+        if (isFarsi) 19.sp else 18.sp
+    }
 
     Column(
         modifier = modifier,
@@ -67,19 +56,13 @@ fun PoemView(
             )
 
             Divider(
-                modifier = Modifier.width(maxWidth),
+                modifier = Modifier.width(304.dp),
                 thickness = 1.dp,
                 color = MaterialTheme.colorScheme.onSurface
             )
         }
 
         Column(
-            modifier = Modifier
-                .onGloballyPositioned { coordinates ->
-                    maxWidth = with(density) {
-                        coordinates.size.width.toDp() + 96.dp
-                    }
-                },
             horizontalAlignment = Alignment.CenterHorizontally
         ){
             Spacer(modifier = Modifier.height(16.dp))

061f55dc1ba5102f32127984b2afe7ada7998d1d
deghat-farhad <deghat.farhad@gmail.com>
2024-04-23 22:15:14 +0330

    release version 1.1.1

--- buildSrc/src/main/java/com/vuxur/khayyam/ConfigurationData.kt
+++ buildSrc/src/main/java/com/vuxur/khayyam/ConfigurationData.kt
@@ -6,8 +6,8 @@ object ConfigurationData {
     const val buildToolsVersion = "34.0.0"
     const val minSdk = 21
     const val targetSdk= 34
-    const val versionCode = 4
-    const val versionName = "1.1.0"
+    const val versionCode = 5
+    const val versionName = "1.1.1"
     const val useSupportLibrary = true
     const val multiDexEnabled = true
     const val isMinifyEnabled = true

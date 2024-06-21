bdd246107b026e4dfc053716627dedcdd7456d6a
deghat-farhad <53361451+deghat-farhad@users.noreply.github.com>
2024-06-21 15:00:54 +0330

    "ImageFileOutputStreamProviderImpl" & "ShareIntentProviderImpl" unit tests (#127)

---
+++ app/src/test/java/com/vuxur/khayyam/pages/poemList/view/viewModel/ShareIntentProviderImplTest.kt
@@ -0,0 +1,44 @@
+package com.vuxur.khayyam.pages.poemList.view.viewModel
+
+import android.content.Intent
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ShareIntentProviderImplTest() {
+    private val shareIntentProvider = ShareIntentProviderImpl()
+    private val mockkIntent: Intent = mockk()
+
+    @BeforeEach
+    fun setup() {
+        mockkConstructor(Intent::class)
+
+        every { anyConstructed<Intent>().setAction(any()) } returns mockkIntent
+        every { mockkIntent.addFlags(any()) } returns mockkIntent
+        every { mockkIntent.setType(any()) } returns mockkIntent
+    }
+
+    @Test
+    fun `getShareTextIntent normal case`() {
+        val shareTextIntent = shareIntentProvider.getShareTextIntent()
+
+        verify { anyConstructed<Intent>().setAction(Intent.ACTION_SEND) }
+        verify { mockkIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION) }
+        verify { mockkIntent.setType("text/plain") }
+        assertEquals(mockkIntent, shareTextIntent)
+    }
+
+    @Test
+    fun `getShareImageIntent normal case`() {
+        val shareImageIntent = shareIntentProvider.getShareImageIntent()
+
+        verify { anyConstructed<Intent>().setAction(Intent.ACTION_SEND) }
+        verify { mockkIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION) }
+        verify { mockkIntent.setType("image/*") }
+        assertEquals(mockkIntent, shareImageIntent)
+    }
+}

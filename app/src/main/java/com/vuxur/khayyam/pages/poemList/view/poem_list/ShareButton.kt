b2af3470d63e31dfe1e8c88b3748e6bfe54be627
deghat-farhad <deghat.farhad@gmail.com>
2024-07-15 11:30:57 +0330

    share menu position fix

--- app/src/main/java/com/vuxur/khayyam/pages/poemList/view/poem_list/ShareButton.kt
+++ app/src/main/java/com/vuxur/khayyam/pages/poemList/view/poem_list/ShareButton.kt
@@ -1,6 +1,8 @@
 package com.vuxur.khayyam.pages.poemList.view.poem_list
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.TextSnippet
 import androidx.compose.material.icons.filled.CopyAll
 import androidx.compose.material.icons.filled.Image
 import androidx.compose.material.icons.filled.Share
@@ -27,71 +29,72 @@ fun ShareButton(
     onShareImage: () -> Unit,
 ) {
     var expanded by remember { mutableStateOf(false) }
+    Box {
+        FloatingActionButton(
+            modifier = modifier,
+            onClick = { expanded = true }
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Share,
+                contentDescription = stringResource(R.string.share)
+            )
+        }
 
-    FloatingActionButton(
-        modifier = modifier,
-        onClick = { expanded = true }
-    ) {
-        Icon(
-            imageVector = Icons.Filled.Share,
-            contentDescription = stringResource(R.string.share)
-        )
-    }
-
-    DropdownMenu(
-        expanded = expanded,
-        onDismissRequest = { expanded = false },
-    ) {
-        DropdownMenuItem(
-            onClick = {
-                onCopyText()
-                expanded = false
-            },
-            text = {
-                Text(
-                    text = stringResource(id = R.string.copyText)
-                )
-            },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Filled.CopyAll,
-                    contentDescription = stringResource(id = R.string.copyText)
-                )
-            }
-        )
-        DropdownMenuItem(
-            onClick = {
-                onShareText()
-                expanded = false
-            },
-            text = {
-                Text(
-                    text = stringResource(id = R.string.shareText)
-                )
-            },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Filled.TextSnippet,
-                    contentDescription = stringResource(id = R.string.shareText)
-                )
-            }
-        )
-        DropdownMenuItem(
-            onClick = {
-                onShareImage()
-                expanded = false
-            },
-            text = {
-                Text(
-                    text = stringResource(id = R.string.shareImage)
-                )
-            },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Filled.Image,
-                    contentDescription = stringResource(id = R.string.shareImage)
-                )
-            }
-        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        ) {
+            DropdownMenuItem(
+                onClick = {
+                    onCopyText()
+                    expanded = false
+                },
+                text = {
+                    Text(
+                        text = stringResource(id = R.string.copyText)
+                    )
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.CopyAll,
+                        contentDescription = stringResource(id = R.string.copyText)
+                    )
+                }
+            )
+            DropdownMenuItem(
+                onClick = {
+                    onShareText()
+                    expanded = false
+                },
+                text = {
+                    Text(
+                        text = stringResource(id = R.string.shareText)
+                    )
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.TextSnippet,
+                        contentDescription = stringResource(id = R.string.shareText)
+                    )
+                }
+            )
+            DropdownMenuItem(
+                onClick = {
+                    onShareImage()
+                    expanded = false
+                },
+                text = {
+                    Text(
+                        text = stringResource(id = R.string.shareImage)
+                    )
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Image,
+                        contentDescription = stringResource(id = R.string.shareImage)
+                    )
+                }
+            )
+        }
     }
 }

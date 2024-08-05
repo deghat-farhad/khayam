eb8f627a0a18151bcf393906f3db252e9b4911d3
deghat-farhad <53361451+deghat-farhad@users.noreply.github.com>
2024-08-05 18:16:36 +0330

    scrollable setting screen (#168)

--- app/src/main/java/com/vuxur/khayyam/pages/setting/view/SettingScreen.kt
+++ app/src/main/java/com/vuxur/khayyam/pages/setting/view/SettingScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
@@ -49,6 +51,7 @@ fun SettingScreen(
     onUseMatchSystemLanguageTranslation: () -> Unit,
 ) {
     var dropdownExpanded by remember { mutableStateOf(false) }
+    val scrollState = rememberScrollState()
 
     Scaffold(
         topBar = {
@@ -70,6 +73,7 @@ fun SettingScreen(
         Surface {
             Column(
                 modifier = Modifier
+                    .verticalScroll(state = scrollState)
                     .fillMaxSize()
                     .padding(paddingValues)
                     .padding(16.dp)
